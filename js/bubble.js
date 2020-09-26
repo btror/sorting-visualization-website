5d4f4790cdce9676587bd098668d3bdba0736692
Brandon <ticer1999@gmail.com>
2020-09-26 02:23:06 -0400

    added code snippets for bubblesort

--- js/bubble.js
+++ js/bubble.js
@@ -7,9 +7,47 @@ ctx.beginPath();
 ctx.fillStyle = "black";
 ctx.font="17px Verdana";
 
+function showCode() {
+    var code = "\n" 
+    + " 1  // bubble sort\n" 
+    + " 2  for (var i = 0; i < list.length - 1; i++) {\n"
+    + " 3     for (var j = 0; j < list.length - i - 1; j++) {\n"
+    + " 4        if (list[j] > list[j + 1]) {\n"
+    + " 5           var temp = list[j];\n"
+    + " 6           list[j] = list[j + 1];\n"
+    + " 7           list[j + 1] = temp;\n"
+    + " 8        }\n"
+    + " 9     }\n"
+    + "10  }";
+    document.getElementById("code-javascript").innerHTML = code;
+
+    code = "\n" 
+    + " 1  // bubble sort\n" 
+    + " 2  for (int i = 0; i < list.length - 1; i++) {\n"
+    + " 3     for (int j = 0; j < list.length - i - 1; j++) {\n"
+    + " 4        if (list[j] > list[j + 1]) {\n"
+    + " 5           int temp = list[j];\n"
+    + " 6           list[j] = list[j + 1];\n"
+    + " 7           list[j + 1] = temp;\n"
+    + " 8        }\n"
+    + " 9     }\n"
+    + "10  }";
+    document.getElementById("code-java").innerHTML = code;
+
+    code = "\n" 
+    + " 1  # bubble sort\n" 
+    + " 2  for i in range(len(list) - 1):\n"
+    + " 3     for j in range(0, n - i - 1):\n"
+    + " 4        if list[j] > list[j + 1]:\n"
+    + " 5           list[j], list[j + 1] = list[j + 1], list[j]\n";
+    document.getElementById("code-python").innerHTML = code;
+
+}
+
 
 
 async function bubbleSort() {
+    showCode();
     //disable randomize button
     disableButtons();
     for (let i = 0; i < list.length - 1; i++) {
